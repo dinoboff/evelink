b745113885257e593936817e1a37c7f3a042575d
Damien Lebrun <dinoboff@hotmail.com>
2013-11-17 02:29:35 +0100

    Let Travis-ci run Google App Engine tests
    Travis build downloads the GAE sdk and can now run the GAE test. To
    allow the same tests to run on python2.6 and 2.7, this commit include a
    test runner script similare to nosegae except it doesn't break on
    python 2.6.
    Nosetests should still be used for development since it collects logs
    and display them on error.

---
+++ runtests.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#
+# Test runner for Travis
+# 
+
+import sys
+import unittest2
+
+GAE_SDK_PATH = './vendors/google_appengine/'
+TEST_PATH = './tests/'
+
+
+def setup_gae(sdk_path):
+    if sys.version_info < (2, 7,):
+        return
+
+    sys.path.insert(0, sdk_path)
+    import dev_appserver
+    dev_appserver.fix_sys_path()
+
+def main(sdk_path, test_path):
+    setup_gae(sdk_path)
+    suite = unittest2.loader.TestLoader().discover(test_path)
+    unittest2.TextTestRunner(verbosity=2, buffer=True).run(suite)
+
+if __name__ == '__main__':
+    main(GAE_SDK_PATH, TEST_PATH)
